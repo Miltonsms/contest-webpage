a79ce22870ce51eea501be2937fd9218e4ac75da
Sergio Merida <sergiotabarini@hotmail.com>
2015-01-03 16:45:58 -0600

    Design

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/**
+* Save all the videoId in id
+*/
+id = [];
+stringUrl="";
+$(document).ready(function() {
+	$('.dropdown-toggle').dropdown()
+});
+
+$(document).ready(function($) {
+	$.ajax({
+		url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=10&format=json",
+		dataType : "jsonp",
+		success : function(parsed_json) {
+	
+		for (var i = 0; i <= 9; i++) {
+			if (i==9){
+				stringUrl = stringUrl + parsed_json["items"][i]["id"]["videoId"];
+			}else{
+				stringUrl = parsed_json["items"][i]["id"]["videoId"]+ "%2C+" +stringUrl
+			};		
+		};
+		console.log(stringUrl);
+		}
+	});
+});
